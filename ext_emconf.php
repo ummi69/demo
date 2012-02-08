240dabaa69a84e5624e09c92da2dbd3c2f611b38
ummi69 <urmas@ummisoft.de>
2012-02-08 17:23:25 +0100

    commit 1

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "demo".
+#
+# Auto generated 08-02-2012 16:54
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'demo',
+	'description' => 'demo for github',
+	'category' => 'plugin',
+	'author' => 'urmas ala',
+	'author_email' => 'urmas@ummisoft.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"5d0d";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"dcd9";s:14:"ext_tables.php";s:4:"be35";s:13:"locallang.xml";s:4:"fbce";s:16:"locallang_db.xml";s:4:"5435";s:19:"doc/wizard_form.dat";s:4:"a45c";s:20:"doc/wizard_form.html";s:4:"e307";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:25:"pi1/class.tx_demo_pi1.php";s:4:"fe16";s:33:"pi1/class.tx_demo_pi1_wizicon.php";s:4:"0f29";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"17b5";}',
+);
+
+?>
